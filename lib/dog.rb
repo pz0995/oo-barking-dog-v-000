bbc600bcbb5df77583d4514452227558c94c48c0
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-15 23:59:40 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Dog
+
+    def name=(name)
+      @name = name
+    end
+
+def name(name="Fido")
+    name
+  end
+
+
+    def bark
+      puts "woof!"
+    end
+end
+
+
+  name = Dog.new
